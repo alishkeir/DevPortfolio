3bf2cfb228a00c7ef7ed9f8e3f3433f655ac9eb4
Ali Shokair <alishkeir_@hotmail.com>
2023-12-24 18:26:06 +0200

    Updated Header to support react-icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,19 @@
+import {
+  FaBlog,
+  FaBriefcase,
+  FaCodepen,
+  FaEnvelopeOpenText,
+  FaFileLines,
+  FaGithub,
+  FaLaptopCode,
+  FaLinkedinIn,
+  FaPaperPlane,
+  FaStackOverflow,
+  FaSun,
+  FaTwitter,
+  FaUser,
+} from 'react-icons/fa6';
+
 const Header = () => {
   return (
     <header className='header text-center'>
@@ -36,118 +52,89 @@ const Header = () => {
               <ul className='social-list list-inline py-2 mx-auto'>
                 <li className='list-inline-item'>
                   <a href='#'>
-                    <i className='fab fa-twitter fa-fw'></i>
+                    <FaTwitter />
                   </a>
                 </li>
                 <li className='list-inline-item'>
                   <a href='#'>
-                    <i className='fab fa-linkedin-in fa-fw'></i>
+                    <FaLinkedinIn />
                   </a>
                 </li>
                 <li className='list-inline-item'>
                   <a href='#'>
-                    <i className='fab fa-github-alt fa-fw'></i>
+                    <FaGithub />
                   </a>
                 </li>
                 <li className='list-inline-item'>
                   <a href='#'>
-                    <i className='fab fa-stack-overflow fa-fw'></i>
+                    <FaStackOverflow />
                   </a>
                 </li>
                 <li className='list-inline-item'>
                   <a href='#'>
-                    <i className='fab fa-codepen fa-fw'></i>
+                    <FaCodepen />
                   </a>
                 </li>
               </ul>
-              <hr />
             </div>
 
             <ul className='navbar-nav flex-column text-start'>
               <li className='nav-item'>
                 <a className='nav-link active' href='index.html'>
-                  <i className='fas fa-user fa-fw me-2'></i>About Me
-                  <span className='sr-only'>(current)</span>
+                  <FaUser className='me-2' />
+                  About Me
+                  {/* <span className='sr-only'>(current)</span> */}
                 </a>
               </li>
               <li className='nav-item'>
                 <a className='nav-link' href='portfolio.html'>
-                  <i className='fas fa-laptop-code fa-fw me-2'></i>Portfolio
+                  <FaLaptopCode className='me-2' />
+                  Portfolio
                 </a>
               </li>
               <li className='nav-item'>
                 <a className='nav-link' href='services.html'>
-                  <i className='fas fa-briefcase fa-fw me-2'></i>Services &amp;
-                  Pricing
+                  <FaBriefcase className='me-2' />
+                  Services &amp; Pricing
                 </a>
               </li>
               <li className='nav-item'>
                 <a className='nav-link' href='resume.html'>
-                  <i className='fas fa-file-alt fa-fw me-2'></i>Resume
+                  <FaFileLines className='me-2' />
+                  Resume
                 </a>
               </li>
               <li className='nav-item'>
                 <a className='nav-link' href='blog-home.html'>
-                  <i className='fas fa-blog fa-fw me-2'></i>Blog
+                  <FaBlog className='me-2' />
+                  Blog
                 </a>
               </li>
               <li className='nav-item'>
                 <a className='nav-link' href='contact.html'>
-                  <i className='fas fa-envelope-open-text fa-fw me-2'></i>
+                  <FaEnvelopeOpenText className='me-2' />
                   Contact
                 </a>
               </li>
-              <li className='nav-item dropdown'>
-                <a
-                  className='nav-link dropdown-toggle'
-                  href='#'
-                  id='navbarDropdown'
-                  role='button'
-                  data-bs-toggle='dropdown'
-                  aria-expanded='false'
-                >
-                  <i className='fas fa-cogs fa-fw me-2'></i>More Pages
-                </a>
-                <ul className='dropdown-menu' aria-labelledby='navbarDropdown'>
-                  <li>
-                    <a className='dropdown-item' href='project.html'>
-                      Project Page
-                    </a>
-                  </li>
-                  <li>
-                    <a className='dropdown-item' href='blog-home.html'>
-                      Blog Home 1
-                    </a>
-                  </li>
-                  <li>
-                    <a className='dropdown-item' href='blog-home-alt.html'>
-                      Blog Home 2
-                    </a>
-                  </li>
-                  <li>
-                    <a className='dropdown-item' href='blog-post.html'>
-                      Blog Post
-                    </a>
-                  </li>
-                </ul>
-              </li>
             </ul>
 
-            <div className='my-2'>
+            <div className='mb-2 mt-4'>
               <a
                 className='btn btn-primary'
                 href='contact.html'
                 target='_blank'
                 rel='noreferrer'
               >
-                <i className='fas fa-paper-plane me-2'></i>Hire Me
+                <FaPaperPlane className='me-2' />
+                Hire Me
               </a>
             </div>
 
             <div className='dark-mode-toggle text-center w-100'>
-              <hr className='mb-4' />
-              <h4 className='toggle-name mb-3 '>
-                <i className='fas fa-adjust me-1'></i>Dark Mode
+              <h4 className='toggle-name mb-3 mt-4 '>
+                <i className='fas fa-adjust me-1'></i>
+                <FaSun className='me-1' />
+                Dark Mode
               </h4>
 
               <input className='toggle' id='darkmode' type='checkbox' />
